aeedf47b42406d9411d347b77954bebd7bdf9ce6
Arpit <arpit7soni@gmail.com>
2026-07-21 02:35:17 +0530

    Time: 3 ms (88.02%), Space: 51.2 MB (95.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,28 +1,35 @@
 class Solution {
-    static int count;
-    static int[][] visited;
     public int numIslands(char[][] grid) {
-        count =0;
-        visited = new int[grid.length][grid[0].length];
+        int m = grid.length; //rows
+        int n = grid[0].length; //cols
+        int visited [][] = new int[m][n];
+        int islands=0;
 
-        for(int i=0; i< grid.length; i++){
-            for(int j=0;j<grid[0].length; j++){
-                if(grid[i][j]=='1' && visited[i][j]==0){
-                    count++;
-                    dfs(i,j,grid);
+        for(int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if(grid[i][j]=='1' && visited[i][j] != 1){
+                    dfs(i,j,visited,grid);
+                    islands++;
                 }
             }
         }
-        return count;
+        return islands;
     }
-    public void dfs(int i , int j , char[][]grid){
-        if(i<0 || i>=grid.length ||j<0|| j>= grid[0].length || grid[i][j] =='0' || visited[i][j] == 1) return;
-        
-        visited[i][j] = 1;
+    public void dfs(int i, int j , int[][] visited,char[][] grid){
+        if(visited[i][j]==1) return;
+        visited[i][j]=1;
 
-        dfs(i-1,j,grid);
-        dfs(i,j-1,grid);
-        dfs(i+1,j,grid);
-        dfs(i,j+1,grid);
-    }    
+        if(i>=0 && j-1>=0 && i<grid.length && j-1<grid[0].length && grid[i][j-1]=='1'){
+            dfs(i,j-1,visited,grid);
+        }
+        if(i-1>=0 && j>=0 && i-1<grid.length && j<grid[0].length && grid[i-1][j]=='1'){
+            dfs(i-1,j,visited,grid);
+        }
+        if(i+1>=0 && j>=0 && i+1<grid.length && j<grid[0].length && grid[i+1][j]=='1'){
+            dfs(i+1,j,visited,grid);
+        }
+        if(i>=0 && j+1>=0 && i<grid.length && j+1<grid[0].length && grid[i][j+1]=='1'){
+            dfs(i,j+1,visited,grid);
+        }
+    }
 }
