8a61b3b2a0ae6399303cff44345ccf883bbddd23
Arpit <arpit7soni@gmail.com>
2026-08-06 12:18:36 +0530

    Time: 1 ms (100%), Space: 42.6 MB (66.49%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -6,7 +6,7 @@ class Solution {
             int prod = 1;
             while(i>0){
                 prod *= i%10;
-                System.out.print(i+ "  ");
+                // System.out.print(i+ "  ");
                 i =i/10;
             }
             if(prod%t==0) return temp;
